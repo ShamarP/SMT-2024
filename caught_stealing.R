65f841437b3d532fece135ec9cb8ff757f77160c
Shamar Phillips <shamarphillips@hotmail.com>
2024-07-03 11:24:27 -0400

    Updated dataframe with information on if it was a steal or not

---
+++ caught_stealing.R
@@ -0,0 +1,83 @@
+## Andy Hutchison
+## SMT 2024
+
+## I'm gonna try and filter to have only stolen bases
+## filtered_game_events from Shamar's Data_prepping
+library(tidyverse)
+library(dplyr)
+library(arrow)
+## creating a vector of all play_id that have a steal occur and then using this
+## to filter out. Also need game_str since same play_id can occur in different games
+
+source("SMT_Data_starter.R")
+
+filtered_game_events <- read_parquet("Potential_Steals/game_events.parquet")
+filtered_game_info <- read_parquet("Potential_Steals/game_info.parquet")
+
+# Most common steal sequences without many bounces
+steal_sequences <- list(c(1, 2, 2, 4), c(1, 2, 2, 5), c(1, 2, 2, 6),
+                        c(1, 255, 2, 2, 4), c(1, 255, 2, 2, 5), c(1, 255, 2, 2, 6),
+                        c(1, 2, 2, 255, 255, 4), c(1, 2, 2, 255, 255, 5), c(1, 2, 2, 255, 255, 6),
+                        c(1, 255, 2, 2, 255, 4), c(1, 255, 2, 2, 255, 5), c(1, 255, 2, 2, 255, 6))
+
+
+steal_sequence_check <- function(events, steal_sequences){
+  any(sapply(steal_sequences, function(sequen) all(sequen %in% events)))
+}
+
+filter_steals <- filtered_game_events %>%
+  group_by(game_str, play_id) %>%
+  summarise(event_sequence = list(player_position), .groups = "drop") %>%
+  mutate(sequence_match = sapply(event_sequence, function(events) steal_sequence_check(events, steal_sequences))) %>%
+  filter(sequence_match) %>%
+  select(game_str, play_id)
+
+# At this point there are some 10s in here (batter plays) and if the batter does
+# something it's not a steal so we need to take that out.
+# But almost steals is plays where one of steal_sequences shows up, could be with
+# other stuff in there
+almost_steals <- filtered_game_events %>%
+  inner_join(filter_steals, by = c("game_str", "play_id"))
+
+# steals is just all the steal plays that occur
+steals <- almost_steals %>%
+  group_by(game_str, play_id) %>%
+  filter(!any(player_position == 10)) %>%
+  ungroup()
+
+steal_plays <- steals %>% distinct(game_str,play_id)
+colnames(steal_plays)[2] <- "play_per_game"
+steal_plays
+following_play <- steal_plays
+following_play$play_per_game <- following_play$play_per_game + 1
+
+
+steal_att <- semi_join(filtered_game_info,steal_plays,by=c("game_str","play_per_game"))
+game_info <- game_info %>% as_tibble()
+post_steal <- semi_join(game_info,following_play,by=c("game_str","play_per_game"))
+steal <- c()
+
+post_steal <- post_steal %>%  mutate(play_per_game = play_per_game - 1)
+pre_steal <- steal_att %>% inner_join(post_steal, by = c("game_str","play_per_game"))
+pre_steal
+
+for(i in 1:nrow(pre_steal)){
+  steal_val <- 0
+  if(!is.na(pre_steal[i,]$first_baserunner.x)){
+    if(!is.na(pre_steal[i,]$second_baserunner.y)){
+      steal_val <- 1
+    }
+  }
+  if(!is.na(pre_steal[i,]$second_baserunner.x)){
+    if(!is.na(pre_steal[i,]$third_baserunner.y)){
+      steal_val <- 1
+    }
+  }
+  steal <- c(steal,steal_val)
+}
+
+steal
+pre_steal$valid_steal <- steal
+
+view(pre_steal)
+
